d0189211547da02f46aaa27675e85793fb994b69
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2024-03-06 11:44:22 +0000

    Base project with maven

---
+++ src/main/java/com/dgc/App.java
@@ -0,0 +1,13 @@
+package com.dgc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
